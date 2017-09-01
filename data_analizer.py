d6c6d58fed40c157fcd0d609087777b39630abc2
h@di <hadisafari76@gmail.com>
2017-09-01 16:07:57 +0430

    add Affinity clustering algorithm

--- data_analizer.py
+++ data_analizer.py
@@ -35,7 +35,7 @@ def draw_graph(graph, position, title = "", labels = None, partition_of_node = N
         color_secret = partition + 2
         color = [color_secret, color_secret * color_secret, color_secret * color_secret * color_secret]
         color = "#" + "0".join([str(i % 10) for i in color]) + "0"
-        nx.draw_networkx_nodes(graph, position, nodes_of_partition[partition], 2000, color, alpha = 0.8)
+        nx.draw_networkx_nodes(graph, position, nodes_of_partition[partition], 200, color, alpha = 0.8)
     nx.draw_networkx_edges(graph, position, alpha = 0.5)
     nx.draw_networkx_labels(graph, position, labels, font_size = 5, font_color = [1, 1, 1])
     if title:
@@ -47,26 +47,35 @@ if __name__ == '__main__':
     users = dict((int(key), users[key]) for key in users.keys())
     followings = dict((int(key), followings[key]) for key in followings.keys())
 
-    # print(translate_followings_db_ids_to_names(followings, users))
-
     graph = convert_dict_to_grpah(followings)
 
-    # position = nx.spring_layout(graph)
-    position = nx.circular_layout(graph)
+    position = nx.spring_layout(graph)
+    # position = nx.circular_layout(graph)
     draw_graph(graph, position, "@" + user_name, users)
     plt.axis("off")
-    # plt.savefig(user_name + ".png")
+    plt.savefig(user_name + ".png")
     plt.show()
 
     kClusters = 2
+    clusters = defaultdict(list)
     clusterers = {
         "Agglomerative": cluster.AgglomerativeClustering(linkage="ward"),
+        "k-Agglomerative": cluster.AgglomerativeClustering(linkage="ward", n_clusters=kClusters),
         "Spectral": cluster.SpectralClustering(n_clusters=kClusters, affinity="precomputed", n_init=200),
         "KMeans": cluster.KMeans(n_clusters=kClusters, n_init=200),
+        "Affinity": cluster.affinity_propagation(S=[[graph.nodes()[j] in graph.neighbors(graph.nodes()[i]) for j in range(len(graph.nodes()))] for i in range(len(graph.nodes()))], max_iter=200, damping=0.6)
     }
     for (clusterer_name, clusterer) in clusterers.items():
-        clusterer.fit([[graph.nodes()[j] in graph.neighbors(graph.nodes()[i]) for j in range(len(graph.nodes()))] for i in range(len(graph.nodes()))])
-        draw_graph(graph, position, "@" + user_name + ": " + clusterer_name, users, dict((graph.nodes()[i], clusterer.labels_[i]) for i in range(len(graph.nodes()))))
+        if clusterer_name == "Affinity":
+            clustering_result = clusterer[1]
+        else:
+            clusterer.fit([[graph.nodes()[j] in graph.neighbors(graph.nodes()[i]) for j in range(len(graph.nodes()))] for i in range(len(graph.nodes()))])
+            clustering_result = clusterer.labels_
+        draw_graph(graph, position, "@" + user_name + ": " + clusterer_name, users, dict((graph.nodes()[i], clustering_result[i]) for i in range(len(graph.nodes()))))
+        clusters[clusterer_name] = [[users[graph.nodes()[i]] for i in range(len(clustering_result)) if clustering_result[i] == j] for j in range(len(set(clustering_result)))]
         plt.axis("off")
         plt.show()
+    
+    print(json.dumps(clusters, indent = 4))
+    
 
