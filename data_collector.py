892a493e3ace027232be545dfcc46b547744a2cf
h@di <hadisafari76@gmail.com>
2017-09-01 06:40:10 +0430

    add log to user to collect_followings_relations function

--- data_collector.py
+++ data_collector.py
@@ -16,35 +16,46 @@ def collect_followings_relations(starting_user_id, max_level, tweepy_api):
     followings = defaultdict(list)
 
     for level in range(max_level):
+        print("level %d (%d users)" % (level, len(last_stage)), file = sys.stderr)
         new_stage = set()
+        index = 0
         for user in last_stage:
+            index += 1
+            print("\r\tproccessing %d of %d (%.2f%%)..." % (index, len(last_stage), index / len(last_stage) * 100), end = "", file = sys.stderr)
+            sys.stderr.flush()
             user_followings = tweepy_api.friends_ids(user)
             followings[user] = user_followings
             new_stage = new_stage.union(set(user_followings))
         stage = stage.union(last_stage)
         last_stage = new_stage.difference(stage)
-        print("level %d : stage size: %d" % (level, len(stage)), file = sys.stderr)
+        print("\r\t                                       \rstage size: %d\n" % (len(stage)), file = sys.stderr)
 
     stage = stage.union(last_stage)
-    print("level %d : stage size: %d" % (max_level, len(stage)), file = sys.stderr)
+    print("level %d (%d users)" % (max_level, len(last_stage)), file = sys.stderr)
+    index = 0
     for user in last_stage:
+        index += 1
+        print("\r\tproccessing %d of %d (%.2f%%)..." % (index, len(last_stage), index / len(last_stage) * 100), end = "", file = sys.stderr)
         user_followings = tweepy_api.friends_ids(user)
         for following in user_followings:
             if following in stage:
                 followings[user].append(following)
+    print("\r\t                                       \rstage size: %d\n" % (len(stage)), file = sys.stderr)
 
+    print("final check...", file = sys.stderr)
+    index = 0
     for user in stage:
+        index += 1
+        print("\r\tproccessing %d of %d (%.2f%%)..." % (index, len(stage), index / len(stage) * 100), end = "", file = sys.stderr)
         if user not in followings.keys():
             followings[user] = []
+
+    print("\r\t                                       \rcollecting data completed. stage size: %d\n" % (len(stage)), file = sys.stderr)
 
     return (followings, stage)
 
 
 def translate_followings_db_ids_to_names(followings, users):
-
-    # preproccess data extarcted from json
-    users = dict((int(i), users[i]) for i in users.keys())
-
     result_db = defaultdict(list)
     for key in followings.keys():
         for value in followings[key]:
@@ -55,13 +66,13 @@ def translate_followings_db_ids_to_names(followings, users):
 if __name__ == '__main__':
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit = True)
+    api = tweepy.API(auth, wait_on_rate_limit = False)
 
     user = api.me()
 
-    print("%s (%s) :\t%d" % (user.screen_name, user.name, user.friends_count), file = sys.stderr)
+    print("@%s (%s) :\t%d following(s)\n" % (user.screen_name, user.name, user.friends_count), file = sys.stderr)
     (followings, ids) = collect_followings_relations(user.id, 1, api)
     users = dict((id, api.get_user(id).screen_name) for id in ids)
     
-    print(json.dumps((users, followings)))
+    print(json.dumps((user.screen_name, users, followings)))
 
