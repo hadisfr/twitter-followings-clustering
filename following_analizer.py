d7809c2688281131c238b08c3c2c36365a55ef1e
h@di <hadisafari76@gmail.com>
2017-09-02 20:18:41 +0430

    following number analize tools

---
+++ following_analizer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+from collections import defaultdict
+
+if __name__ == '__main__':
+    (followings_file_address, clusters_file_address) = tuple(input().split(" "))
+
+    with open(followings_file_address) as file:
+        (user_names, followings) = json.loads(file.read())
+    user_names = dict((int(key), value) for (key, value) in user_names.items())
+    user_ids = dict((value, int(key)) for (key, value) in user_names.items())
+    followings = dict((int(key), value) for (key, value) in followings.items())
+    with open(clusters_file_address) as file:
+        (clusers) = json.loads(file.read())["Spectral"]
+
+    sum = 0
+    for (user_id, followings_number) in followings.items():
+        sum += followings_number
+    average_all = sum / len(followings)
+
+    average_by_cluster = []
+    for cluster in clusers:
+        sum = 0
+        for user_name in cluster:
+            sum += followings[user_ids[user_name]]
+        average_by_cluster.append(sum / len(cluster))
+
+    print(json.dumps((average_all, average_by_cluster), indent = 4))
+
+    cat_length = 100
+    cats = defaultdict(list)
+    for (user_id, followings_number) in followings.items():
+        cats[int(followings_number / cat_length)].append(user_names[user_id])
+    print(json.dumps(dict((key, len(cats[key])) for key in sorted(cats, reverse = True)), indent = 4))
+
---
+++ following_counter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+
+import tweepy
+import json
+import sys
+
+
+from keys import *
+
+
+if __name__ == '__main__':
+    users = json.loads(input())[1]
+    users = dict((int(key), users[key]) for key in users.keys())
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit = False)
+
+    followings = dict()
+    for user_id in users.keys():
+        try:
+            print(users[user_id], end = " ", file = sys.stderr)
+            followings[user_id] = api.get_user(user_id).friends_count
+            print(followings[user_id], file = sys.stderr)
+        except Exception as ex:
+            print(ex, file = sys.stderr)
+
+    print(json.dumps((users, followings)))
+
