7554c047cc0a7da868c3db698cc091ed170dc688
h@di <hadisafari76@gmail.com>
2017-09-03 16:25:13 +0430

    minor change in output of following count analizer

--- following_count_analizer.py
+++ following_count_analizer.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     user_ids = dict((value, int(key)) for (key, value) in user_names.items())
     followings = dict((int(key), value) for (key, value) in followings.items())
     with open(clusters_file_address) as file:
-        (clusers) = json.loads(file.read())[clustering_methode]
+        (clusters) = json.loads(file.read())[clustering_methode]
 
     sum = 0
     for (user_id, followings_number) in followings.items():
@@ -21,18 +21,16 @@ if __name__ == '__main__':
     average_all = sum / len(followings)
 
     average_by_cluster = []
-    for cluster in clusers:
+    for cluster in clusters:
         sum = 0
         for user_name in cluster:
             sum += followings[user_ids[user_name]]
         average_by_cluster.append(sum / len(cluster))
-
-    print(json.dumps((average_all, average_by_cluster), indent = 4))
 
     cat_length = 100
     cats = defaultdict(list)
     for (user_id, followings_number) in followings.items():
         cats[int(followings_number / cat_length) * cat_length].append(user_names[user_id])
 
-    print(json.dumps(dict((key, len(cats[key])) for key in sorted(cats, reverse = True)), indent = 4))
+    print(json.dumps(dict({"all": average_all, "by_cluster": average_by_cluster, "by_number": dict((key, len(cats[key])) for key in sorted(cats, reverse = True))}), indent = 4))
 
