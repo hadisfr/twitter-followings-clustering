c6bf71592c274cdee4e736b4eba7d0e115e2d749
h@di <hadisafari76@gmail.com>
2017-09-06 03:21:15 +0430

    turn on wait_on_rate_limit in following_counter

--- following_counter.py
+++ following_counter.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit = False)
+    api = tweepy.API(auth, wait_on_rate_limit = True)
 
     followings = dict()
     for user_id in users.keys():
